4114f7fe3112b9e27e1a3b49efac50baf83a5a81
Roman O. <r.o4agow2010@yandex.ru>
2025-01-18 20:10:10 +0500

    я присвоил значение переменной

---
+++ 3.2. Переменные, константы, литералы/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._2.Переменные__константы__литералы
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            string myName = "Roman";
+            
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
