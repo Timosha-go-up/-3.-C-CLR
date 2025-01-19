a82b2ffac1073ff33dbd1d3164e3f48c17e18a60
Roman O. <r.o4agow2010@yandex.ru>
2025-01-19 23:28:40 +0500

    3.2 + 3.3 редактирование

--- 3.2. Переменные, константы, литералы/Program.cs
+++ 3.2. Переменные, константы, литералы/Program.cs
@@ -1,39 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _3._2.Переменные__константы__литералы
+class MainClass
 {
-    class MainClass
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            //Console.Write("My name is\t");
-            //string MYName = Console.ReadLine();
-           
-            //Console.Write("My age is\t");
-            //string MyAge = Console.ReadLine();
-            
-            //Console.Write("Do i have a pet\t");
-            //string MyPet = Console.ReadLine();
-            
-            //Console.WriteLine("My shoe size is\t");
-            //string MySizeShoue = Console.ReadLine();
 
-            string MyName = "Roman";
-            byte MyAge = 39;
-            bool HaveIApet = true;
-            double MyShoeSize = 45;
+        string myName;
+        myName = "Roman";
 
-            Console.WriteLine("My name is " + MyName);
-            Console.WriteLine("MyAge " + MyAge);
-            Console.WriteLine("Do I have a pet? " + HaveIApet);
-            Console.WriteLine("My shoe size is " + MyShoeSize);
+        Console.WriteLine(myName);
 
-
-            Console.ReadKey();
-        }
+        Console.ReadKey();
     }
 }
